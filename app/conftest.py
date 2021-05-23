3c97f5182dc875bd8d44ffc189af08834a0cb63c
wk0 <willkelly0@gmail.com>
2021-05-23 16:10:18 -0500

    fix import, add logger to tests

--- app/conftest.py
+++ app/conftest.py
@@ -2,9 +2,14 @@
 # https://stackoverflow.com/questions/34466027/in-pytest-what-is-the-use-of-conftest-py-files
 
 import pytest
-from . import env_loader
+from . import config_loader
 
 
 @pytest.fixture()
 def load_env():
-    env_loader()
+    config_loader.config_env()
+
+
+@pytest.fixture()
+def load_logger():
+    config_loader.config_logger("test")
