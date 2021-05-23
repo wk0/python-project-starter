5b3d8f43e8da515f5ab0d3dbad25a316993a1418
wk0 <willkelly0@gmail.com>
2021-05-23 16:42:56 -0500

    added logging test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 # test_config.py
 import os
+import logging
 
 from .context import app
 
@@ -10,3 +11,16 @@ def test_base():
 
 def test_env():
     assert os.getenv("SAMPLE_VAR") == "filler"
+
+
+def test_log(caplog):
+    # https://docs.pytest.org/en/stable/logging.html
+    caplog.set_level(logging.INFO)
+    logmsg = "test write to log"
+
+    logging.info(logmsg)
+
+    log_messages = [rec.message for rec in caplog.records]
+
+    assert len(log_messages) == 1
+    assert log_messages[0] == logmsg
