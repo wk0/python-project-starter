c885287fc55e9290dbdb04cc8e30dd0047cdc82e
wk0 <willkelly0@gmail.com>
2021-05-23 16:54:53 -0500

    init sample submodule

---
+++ app/sample/sample.py
@@ -0,0 +1,7 @@
+class Sample:
+    def __init__(self, sample_var):
+        self.sample_var = sample_var
+        self.sample_list = []
+
+    def sample_func(self, sample_item):
+        self.sample_list.append(sample_item)
