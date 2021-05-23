2b227212f4c3fd7b5caeb8af6fea496663119f2b
wk0 <willkelly0@gmail.com>
2021-05-23 16:55:20 -0500

    import deps, config setup, run sample class

--- app/runner.py
+++ app/runner.py
@@ -1,4 +1,30 @@
 import logging
 import sys
+import os
 
-print("runner running")
+sys.path.append("../")
+
+from app.config_loader import config_env, config_logger
+from app.sample.sample import Sample
+
+config_env()
+config_logger()
+
+
+def run_sample():
+    sample_var = os.getenv("SAMPLE_VAR")
+    logging.info(sample_var)
+
+    sample = Sample(sample_var=sample_var)
+
+    logging.info(sample.sample_var)
+
+    sample.sample_func(sample_item="a")
+    sample.sample_func(sample_item="b")
+    sample.sample_func(sample_item="c")
+
+    logging.info(sample.sample_list)
+
+
+if __name__ == "__main__":
+    run_sample()
